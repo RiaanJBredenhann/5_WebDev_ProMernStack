50e3f823caf7dcaae2931d61746db14afa8c0061
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-11-23 13:59:04 +0200

    modified issuetable and issuerow classes

--- chapter3/public/App.js
+++ chapter3/public/App.js
@@ -52,13 +52,11 @@ var IssueTable = /*#__PURE__*/function (_React$Component2) {
         style: rowStyle
       }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 1,
-        issue_title: "Error in console when clicking Add"
-      }), /*#__PURE__*/React.createElement(IssueRow, {
+        issue_id: 1
+      }, "Error in console when clicking Add"), /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 2,
-        issue_title: "Missing bottom border on panel"
-      })));
+        issue_id: 2
+      }, /*#__PURE__*/React.createElement("div", null, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel"))));
     }
   }]);
   return IssueTable;
@@ -78,7 +76,7 @@ var IssueRow = /*#__PURE__*/function (_React$Component3) {
         style: style
       }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
         style: style
-      }, this.props.issue_title));
+      }, this.props.children));
     }
   }]);
   return IssueRow;
