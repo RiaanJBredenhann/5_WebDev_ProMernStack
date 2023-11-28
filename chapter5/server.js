300af1a4e4f55ec9fffee3d8bcf9a62adf8a4d2e
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-11-28 13:48:24 +0200

    update

--- chapter5/server.js
+++ chapter5/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 
@@ -8,20 +8,20 @@ let aboutMessage = "Issue TRacker API v1.0";
 
 const issuesDB = [
     {
-        id: 1, 
-        status: 'New', 
-        owner: 'Ravan', 
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
         effort: 5,
-        created: new Date('2019-01-15'), 
+        created: new Date('2019-01-15'),
         due: undefined,
         title: 'Error in console when clicking Add',
     },
     {
-        id: 2, 
-        status: 'Assigned', 
-        owner: 'Eddie', 
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
         effort: 14,
-        created: new Date('2019-01-16'), 
+        created: new Date('2019-01-16'),
         due: new Date('2019-02-01'),
         title: 'Missing bottom border on panel',
     },
@@ -34,10 +34,14 @@ const GraphQLDate = new GraphQLScalarType({
         return value.toISOString();
     },
     parseValue(value) {
-        return new Date(value);
+        const dateValue = new Date(value);
+        return isNaN(dateValue) ? undefined : dateValue;
     },
     parseLiteral(ast) {
-        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
+        if (ast.kind == Kind.STRING) {
+            const value = new Date(ast.value);
+            return isNaN(value) ? undefined : value;
+        }
     },
 });
 
@@ -59,6 +63,22 @@ function setAboutMessage(_, { message }) {
 
 function issueList() {
     return issuesDB;
+}
+
+function validateIssue(_, { issue }) {
+    const errors = [];
+
+    if (issue.title.length < 3) {
+        errors.push('Field "title" must be at least 3 characters long.')
+    }
+
+    if (issue.status == 'Assigned' && !issue.owner) {
+        errors.push('Field "owner" is required when status is "Assigned"');
+    }
+    
+    if (errors.length > 0) {
+        throw new UserInputError('Invalid input(s)', { errors });
+    }
 }
 
 function issueAdd(_, { issue }) {
