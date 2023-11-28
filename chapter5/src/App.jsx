932cf5e551feaec3ca061b253ec15b8d20b401a5
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-11-28 13:44:48 +0200

    update

--- chapter5/src/App.jsx
+++ chapter5/src/App.jsx
@@ -120,12 +120,8 @@ class IssueList extends React.Component {
     }
 
     async createIssue(issue) {
-        const query = `mutation {
-            issueAdd(issue:{
-                title: "${issue.title}",
-                owner: "${issue.owner}",
-                due: "${issue.due.toISOString()}",
-            }) {
+        const query = `mutation issueAdd($issue: IssueInputs!) {
+            issueAdd(issue: $issue) {
                 id
             }
         }`;
@@ -133,7 +129,7 @@ class IssueList extends React.Component {
         const response = await fetch('/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query })
+            body: JSON.stringify({ query, variables: { issue } })
         });
         this.loadData();
     }
